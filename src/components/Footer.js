9e48cb59faf8a01b16b4441e524684deb89d42b4
Veronica Andersen <v.andersen98@gmail.com>
2025-08-18 16:16:27 +0200

    Fixed warning in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="bg-zinc-900">
       <footer>
-        <div className="mx-auto px-12 text-white flex justify-center items-center max-w-screen-lg h-16 block">
+        <div className="mx-auto px-12 text-white justify-center items-center max-w-screen-lg h-16 block">
           <p>ByVeronica • © 2025</p>
         </div>
       </footer>
